dc2ae6a7138ca5275ba4f8402cb9810c3bc4302f
Wooyon <48887513+Wooyon@users.noreply.github.com>
2019-11-13 20:36:07 +0900

    #3.6 to do list part two

--- todo.js
+++ todo.js
@@ -4,15 +4,29 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos)); //JSON.stringify는 js object string로 저장되게 만든다.
+}
+
 function paintToDo(text) {
   const li = document.createElement("li");
   const delBtn = document.createElement("button");
-  delBtn.innerText = "X";
   const span = document.createElement("span");
+  const newId = toDos.length + 1;
+  delBtn.innerText = "X";
   span.innerText = text;
   li.appendChild(delBtn);
   li.appendChild(span); // father element안에 넣어주기.
+  li.id = newId;
   toDoList.appendChild(li);
+  const toDoObj = {
+    text: text,
+    id: newId
+  };
+  toDos.push(toDoObj); //push는 배열안에 집어넣는다는 의미.
+  saveToDos();
 }
 
 function handleSubmit(event) {
@@ -23,8 +37,12 @@ function handleSubmit(event) {
 }
 
 function loadToDos() {
-  const toDos = localStorage.getItem(TODOS_LS);
-  if (toDos !== null) {
+  const loadedToDos = localStorage.getItem(TODOS_LS); //포함 아래의 if문은 맨 위까지 함수가 도달하고 난 후 생성된 것들을 새로고침 후 불러오는 역할을 한다.
+  if (loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach(function(toDo) {
+      paintToDo(toDo.text);
+    }); //forEach()는 기본적으로 함수를 실행, array에 담겨있는 것들 각각에 한번씩 함수를 실행시켜준다.
   }
 }
 
